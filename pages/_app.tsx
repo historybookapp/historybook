c07dbd07bb3538ba29f64b021879706b96b865ee
Roy Li <me@dada.li>
2021-02-21 01:09:11 +0800

    fix: remvoe redundant div

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../styles/theme'
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <div>
+  <>
     <Provider session={pageProps.session}>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </Provider>
-  </div>
+  </>
 )
 
 export default App
