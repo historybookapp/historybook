bb035b6f34e23b4082459c8132a52a2db172a9d3
Roy Li <me@dada.li>
2021-02-22 02:13:57 +0800

    feat: change theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -2,9 +2,16 @@ import { extendTheme } from '@chakra-ui/react'
 
 const colors = {
   brand: {
-    blue: '#14213D',
-    orange: '#FCA311',
-    platinum: '#E5E5E5',
+    50: '#ffefdb',
+    100: '#ffd9ad',
+    200: '#fec57e',
+    300: '#fdb54d',
+    400: '#fca71c',
+    500: '#e37f03',
+    600: '#b05600',
+    700: '#7f3500',
+    800: '#4d1a00',
+    900: '#1d0500',
   },
 }
 const theme = extendTheme({ colors })
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        'sb-blue': '#14213D',
-        'sb-orange': '#FCA311',
-        'sb-platinum': '#E5E5E5',
-      },
+      colors: {},
     },
   },
   variants: {
